984921c3f9242403c47d160f03ed6c1ffac1085b
Neha Rose <NehaRose.VembilDenny@flemingcollege.ca>
2023-01-28 23:08:53 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("Simple is better than complex.")
 print("Complex is better than complicated.")
+print("Namespaces are one honking great idea -- let's do more of those!")
